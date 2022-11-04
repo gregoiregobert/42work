42babacbe8dfe603aeaec958328632be7df9150d
Gregoire GOBERT <ggobert@e1r5p1.clusters.42paris.fr>
2022-11-04 12:02:29 +0100

    gnl training

--- training/gnl/gnl.h
+++ training/gnl/gnl.h
@@ -5,24 +5,17 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ggobert <ggobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/01 13:23:25 by ggobert           #+#    #+#             */
-/*   Updated: 2022/11/02 14:55:26 by ggobert          ###   ########.fr       */
+/*   Created: 2022/11/04 11:58:14 by ggobert           #+#    #+#             */
+/*   Updated: 2022/11/04 11:59:28 by ggobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef		GNL_H
-# define	GNL_H
- 
-# include <fcntl.h>
+#ifndef GNL_H
+# define GLN_H
+
 # include <stdio.h>
+# include <fcntl.h>
 # include <unistd.h>
 # include <stdlib.h>
 
-char	*before_nl(char *tmp);
-char	*ft_strdup(char *str);
-char	*ft_strjoin_free(char *str1, char *str2, int free_s1, int free_s2);
-char	*gnl(int fd);
-int		is_nl(char *str);
-void	after_nl(char *tmp, char *rest);
-
-#endif
+#endif 
--- training/gnl/main.c
+++ training/gnl/main.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ggobert <ggobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/01 11:14:48 by ggobert           #+#    #+#             */
-/*   Updated: 2022/11/02 15:36:56 by ggobert          ###   ########.fr       */
+/*   Created: 2022/11/04 11:59:35 by ggobert           #+#    #+#             */
+/*   Updated: 2022/11/04 12:01:44 by ggobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,15 @@
 
 int	main(int ac, char **av)
 {
-	int fd;
-	char *buff;
-	int i;
-	
-	(void)ac;
-	i = -1;
-	buff = 0;
+	int	fd;
+	char	*buff;
+
 	fd = open(av[1], O_RDONLY);
-	while (++i < 50)
+	buff = gnl(fd);
+	while (buff)
 	{
-		buff = gnl(fd);
 		printf("%s", buff);
 		free(buff);
+		buff = gnl(fd);
 	}
-	close(fd);
 }
