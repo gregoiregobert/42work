ef1d9bc9416b5996dd3bea68e5c4e1c51fba8f05
Gregoire GOBERT <ggobert@e1r5p1.clusters.42paris.fr>
2023-01-23 15:01:42 +0100

    add m02 ex02

--- CPP/module_02/ex_02/Fixed.cpp
+++ CPP/module_02/ex_02/Fixed.cpp
@@ -187,4 +187,50 @@ Fixed Fixed::operator--(int)
 //                                                    //
 //****************************************************//
 
-Fixed const Fixed::max(Fixed const &a, Fixed const &b);
+Fixed const &Fixed::max(Fixed const &a, Fixed const &b)
+{
+	if (a > b)
+		return (a);
+	return (b);
+}
+
+Fixed &Fixed::max(Fixed &a, Fixed &b)
+{	
+	if (a > b)
+		return (a);
+	return (b);
+}
+
+Fixed const &max(Fixed const &a, Fixed const &b)
+{
+	return  (Fixed::max(a, b));
+}
+
+Fixed &max(Fixed &a, Fixed &b)
+{	
+	return  (Fixed::max(a, b));
+}
+
+Fixed const &Fixed::min(Fixed const &a, Fixed const &b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
+
+Fixed &Fixed::min(Fixed &a, Fixed &b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
+
+Fixed const &min(Fixed const &a, Fixed const &b)
+{
+	return (Fixed::min(a, b));
+}
+
+Fixed &min(Fixed &a, Fixed &b)
+{
+	return (Fixed::min(a,b));
+}
