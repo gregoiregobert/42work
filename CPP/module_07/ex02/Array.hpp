2f0356b5ea4ed865712199db6dcb80e2b042bd20
Gregoire GOBERT <ggobert@e1r1p9.clusters.42paris.fr>
2023-02-15 14:58:36 +0100

    m07 ex02

--- CPP/module_07/ex02/Array.hpp
+++ CPP/module_07/ex02/Array.hpp
@@ -1,28 +1,38 @@
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
+#include <stdexcept>
+
 template<class T>
 class Array
 {
+	private:
+
+		unsigned int	_n;
+		T				*_array;
+
 	public:
 
 		Array();
-		Array(unsigned int n);
-		Array(const Array& other);
+		Array( unsigned int n );
+		Array( const Array& other );
 		~Array();
 
-		&operator=(const Array& other);
+		Array &operator=( const Array& other );
+		T &operator[]( size_t index );
 
-		int	size();
+		unsigned int	size();
 
-		class WrongIndex : public std::exception
-		{
-			public:
-			virtual const char* what() const throw()
-			{
-				return ("\033[1;31m...........\033[0m");
+	/******************************/
+	/*        Exception(s)        */
+	/******************************/
+
+	class WrongIndex : public std::exception{
+		public:
+			virtual const char* what() const throw(){
+				return ("\033[1;31mError : index does not exist\033[0m");
 			}
-		};
+	};
 };
 
 #endif
