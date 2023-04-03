1bae941a67f15323ce5e152f25440ed6281f4eb9
Votre Nom <Vous@exemple.com>
2023-04-03 15:59:31 +0200

    flemme

--- Inception/mariadb/conf/config.sh
+++ Inception/mariadb/conf/config.sh
@@ -1,13 +1,40 @@
 #!/bin/sh
 
-service mysql start;
+if [ ! -d /var/lib/mysql/wordpress ]
+then
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
+	echo "Starting temporary server..."
+	cd '/usr' ; /usr/bin/mysqld_safe --datadir=/var/lib/mysql &
 
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+	until mysqladmin ping 2> /dev/null; do
+		sleep 2
+	done
+	
+	echo "Creation de la database Wordpress..."
+	mysql -u root <<- _EOF_
+		CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+		CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+		GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+		FLUSH PRIVILEGES;
+	_EOF_
+	echo "Ok"
 
-exec mysqld_safe
+	echo "MYSQL installation..."
+	mysql -u root <<- _EOF_
+		ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+		DELETE FROM mysql.user WHERE User='';
+		DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+		DROP DATABASE IF EXISTS test;
+		DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+		FLUSH PRIVILEGES;
+	_EOF_
+	echo "Ok"
+
+	echo "Stopping temporary server!"
+	mysqladmin --user=root --password=$MYSQL_ROOT_PASSWORD shutdown
+
+	sleep 3
+fi
+
+echo "Starting mariadb server..."
+exec "$@"
