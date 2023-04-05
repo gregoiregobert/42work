3e247303f6dc916a9f9863d863adc89233d70fbb
Votre Nom <Vous@exemple.com>
2023-04-05 13:00:32 +0200

    finish inception

---
+++ Inception/clean_site.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo Azertyuio456+ | sudo -S rm -rf ~/data/mariadb_volume/
+echo Azertyuio456+ | sudo -S rm -rf ~/data/wordpress_volume/
+
+mkdir ~/data/mariadb_volume/
+mkdir ~/data/wordpress_volume/
