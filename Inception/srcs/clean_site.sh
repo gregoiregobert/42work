4d92588fe83f8537d70dac26839bc1b6f54122f1
Votre Nom <Vous@exemple.com>
2023-04-07 18:04:59 +0200

    fini pour de bon

--- Inception/srcs/clean_site.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo Azertyuio456+ | sudo -S rm -rf ~/data/mariadb_volume/
-echo Azertyuio456+ | sudo -S rm -rf ~/data/wordpress_volume/
-
-mkdir ~/data/mariadb_volume/
-mkdir ~/data/wordpress_volume/
