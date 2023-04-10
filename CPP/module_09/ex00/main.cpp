85703e021fe254b5c3c1b4bed2f211f5b6153ce5
Gregoire GOBERT <ggobert@e1r1p9.clusters.42paris.fr>
2023-04-10 17:27:06 +0200

    cpp 09 ex 00

--- CPP/module_09/ex00/main.cpp
+++ CPP/module_09/ex00/main.cpp
@@ -1,10 +1,50 @@
 #include "BitcoinExchange.hpp"
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <queue>
+#include <cctype>
+#include <boost/regex.hpp>
 
-int	main(int av, char ** ac)
+int check_date_format( char *c )
 {
-	if (av != 2)
+	while ( i < 4 )
+		if ( !isdigit( c[i++] ) )
+			return (1);
+
+	if ( c[i++] != '-' )
+		return (1);
+
+	while ( i < 7 )
+		if ( !isdigit( c[i++] ) )
+			return (1);
+
+	if ( c[i++] != '-' )
+		return (1);
+
+	while ( i < 10 )
+		if ( !isdigit( c[i++] ) )
+			return (1);
+
+	if ( c[i++] != ' ' )
+		return (1);
+}
+
+int	check_line( std::string line )
+{
+	int i = 0;
+	const char *c = line.c_str();
+
+
+	return (0);
+}
+
+int	main(int ac, char ** av)
+{
+	std::string line;
+	std::queue<std::string> array;
+
+	if (ac != 2)
 		std::cout << "Error: could not open file." << std::endl;
 
 	std::ifstream ifs(av[1]);
@@ -13,5 +53,15 @@ int	main(int av, char ** ac)
 		std::cout << "Error: could not open file." << std::endl;
 		return (0);
 	}
-	
+	while ( std::getline( ifs, line ) )
+	{
+		if ( !check_line( line ) )
+			array.push( line );
+	}
+	while ( array.size() )
+	{
+		std::cout << array.front() << std::endl;
+		array.pop();
+	}
 }
+
