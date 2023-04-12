bce3238a91ef82bc381fb336ca3d933786a3fb65
Gregoire GOBERT <ggobert@e1r1p9.clusters.42paris.fr>
2023-04-12 15:30:03 +0200

    cpp 09 mnb de jour ??

--- CPP/module_09/ex00/main.cpp
+++ CPP/module_09/ex00/main.cpp
@@ -4,10 +4,14 @@
 #include <sstream>
 #include <queue>
 #include <cctype>
-#include <boost/regex.hpp>
 
-int check_date_format( char *c )
+int	check_line( std::string line )
 {
+	std::string tmp;
+	int nb;
+	int i = 0;
+	const char *c = line.c_str();
+
 	while ( i < 4 )
 		if ( !isdigit( c[i++] ) )
 			return (1);
@@ -18,6 +22,11 @@ int check_date_format( char *c )
 	while ( i < 7 )
 		if ( !isdigit( c[i++] ) )
 			return (1);
+	tmp.append(line, 5, 2);
+	std::istringstream iss(tmp);
+	iss >> nb;
+	if (nb > 12)
+		return (1);
 
 	if ( c[i++] != '-' )
 		return (1);
@@ -26,23 +35,34 @@ int check_date_format( char *c )
 		if ( !isdigit( c[i++] ) )
 			return (1);
 
-	if ( c[i++] != ' ' )
+	if ( c[i++] != ' ' || c[i++] != '|' || c[i++] != ' ')
 		return (1);
-}
 
-int	check_line( std::string line )
-{
-	int i = 0;
-	const char *c = line.c_str();
+	if ( c[i++] == '-' )
+	{
+		std::cerr << "Error: not a positive number." << std::endl;
+		return (2);
+	}
 
+	while ( i < (int)line.size() && c[i] != '.' )
+		if ( !isdigit( c[i++] ) )
+			return (1);
+	
+	if ( c[i] == '.' && i+1 != (int)line.size())
+		i++;
+
+	while ( i < (int)line.size() )
+		if ( !isdigit( c[i++] ) )
+			return (1);
 
 	return (0);
 }
 
 int	main(int ac, char ** av)
 {
+	int ret;
 	std::string line;
-	std::queue<std::string> array;
+	std::queue<std::string> queue;
 
 	if (ac != 2)
 		std::cout << "Error: could not open file." << std::endl;
@@ -53,15 +73,18 @@ int	main(int ac, char ** av)
 		std::cout << "Error: could not open file." << std::endl;
 		return (0);
 	}
+
 	while ( std::getline( ifs, line ) )
+		queue.push( line );
+
+	while ( queue.size() )
 	{
-		if ( !check_line( line ) )
-			array.push( line );
-	}
-	while ( array.size() )
-	{
-		std::cout << array.front() << std::endl;
-		array.pop();
+		ret = check_line( queue.front() );
+		if ( !ret )
+			std::cout << queue.front() << std::endl;
+		else if ( ret == 1 )
+			std::cerr << "Error: bad input => " << queue.front() << std::endl;
+		queue.pop();
 	}
 }
 
