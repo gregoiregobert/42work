61d925edb370b47916d1d6ce62d0bb9226ce685a
Gregoire GOBERT <ggobert@e1r1p9.clusters.42paris.fr>
2023-04-25 16:20:24 +0200

    end of 09

--- CPP/module_09/ex02/PMergeMe.cpp
+++ CPP/module_09/ex02/PMergeMe.cpp
@@ -10,7 +10,7 @@ PMergeMe::PMergeMe( int ac, char **av ) : _ac(ac) ,_av(av)
 	// std::cout << "PMergeMe constructor called" << std::endl;
 	if (ac < 3)
 		throw PMergeMe::NoArg();
-	put_in_array();
+	put_in_list();
 }
 
 PMergeMe::PMergeMe( const PMergeMe& other )
@@ -39,10 +39,90 @@ PMergeMe	&PMergeMe::operator=( const PMergeMe &other )
 //                    Function(s)                     //
 //****************************************************//
 
-void	PMergeMe::put_in_array()
+void	PMergeMe::put_in_list()
 {
+	std::list<int> list;
+
 	for (int i = 1; i < _ac; i++)
 	{
-		std::atoi(_av[i]);
+		if (std::atoi(_av[i]) < 1)
+			throw BadInput();
+		list.push_back(std::atoi(_av[i]));
 	}
+
+	std::cout << "Before : ";
+	for (std::list<int>::iterator it = list.begin(); it != list.end(); it++)
+		std::cout <<*it << " ";
+	std::cout << std::endl;
+
+	list = recursive_sort(list);
+
+	std::cout << "After : ";
+	for (std::list<int>::iterator it = list.begin(); it != list.end(); it++)
+		std::cout <<*it << " ";
+	std::cout << std::endl;
+}
+
+std::list<int>	PMergeMe::recursive_sort(std::list<int> array)
+{
+	if (array.size() > 2)
+	{
+		std::list<int> left;
+		std::list<int> right;
+		int i = 0;
+
+		for (std::list<int>::iterator it = array.begin(); it != array.end(); it++)
+		{
+			if (i < (int)array.size() / 2)
+				left.push_back(*it);
+			else
+				right.push_back(*it);
+			i++;
+		}
+		return ( insert( recursive_sort( left ), recursive_sort( right ) ) );
+	}
+	else
+	{
+		if (array.size() > 1)
+			if (array.front() > array.back())
+				array.reverse();
+		return (array);
+	}
+}
+
+std::list<int>	PMergeMe::insert(std::list<int> left, std::list<int> right)
+{
+	std::list<int> ret;
+	
+	while (left.size() && right.size())
+	{
+		if (left.front() < right.front())
+		{
+			ret.push_back(left.front());
+			left.pop_front();
+		}
+		else
+		{
+			ret.push_back(right.front());
+			right.pop_front();
+		}
+	}
+
+	if (left.size() && !right.size())
+	{
+		while (left.size())
+		{
+			ret.push_back(left.front());
+			left.pop_front();
+		}
+	}
+	if (!left.size() && right.size())
+	{
+		while (right.size())
+		{
+			ret.push_back(right.front());
+			right.pop_front();
+		}
+	}
+	return (ret);
 }
--- CPP/module_09/ex02/PMergeMe.hpp
+++ CPP/module_09/ex02/PMergeMe.hpp
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <sstream>
 #include <cctype>
-#include <cstddef>
+#include <cstdlib>
 
 class PMergeMe
 {
@@ -23,13 +23,21 @@ class PMergeMe
 
 		PMergeMe &operator=( const PMergeMe& other);
 
-		void	put_in_array();
-		void	check_format();
+		void			put_in_list();
+		std::list<int>	recursive_sort(std::list<int> array);
+		std::list<int>	insert(std::list<int> left, std::list<int> right);
 
 		class NoArg : public std::exception{
 			public:
 			virtual const char* what() const throw(){
 				return ("\033[1;31mError: Not enough arguments.\033[0m");
+			}
+		};
+
+		class BadInput : public std::exception{
+			public:
+			virtual const char* what() const throw(){
+				return ("\033[1;31mError: Bad Input.\033[0m");
 			}
 		};
 };
