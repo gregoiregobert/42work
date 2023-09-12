35162f4510ae8babf7b5cb5c651b82a9eeb5b1ef
Gregoire GOBERT <ggobert@e1r1p9.clusters.42paris.fr>
2023-09-12 18:02:53 +0200

    dernier chapitre de angular

--- ft_transcendance/export.sh
+++ ft_transcendance/export.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
+if [ ! -d "/goinfre/ggobert/trans_volume" ]; then
+	mkdir /goinfre/ggobert/trans_volume
+fi
+
+
+
+my_copy()
+{
+	while :; do
+		echo -n "copie .\r"
+		sleep 1
+		echo -n "copie ..\r"
+		sleep 1
+		echo -n "copie ...\r"
+		sleep 1
+		echo -n "           \r"
+	done
+}
+
+tput civis
+my_copy &
+
 cp -r snapface /goinfre/ggobert/trans_volume
 
-if [$? -ne 0]; then 
-	echo "erreur: cp n'a pas fonctionné."
-fi
+kill $!
+tput cnorm
