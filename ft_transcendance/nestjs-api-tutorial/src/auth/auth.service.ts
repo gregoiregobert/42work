d0a64dfc9db6c20fa03e87263f6b51fb612d5cf1
Gregoire GOBERT <ggobert@e1r1p9.clusters.42paris.fr>
2023-09-15 18:05:25 +0200

    nestjs tuto 1:09

--- ft_transcendance/nestjs-api-tutorial/src/auth/auth.service.ts
+++ ft_transcendance/nestjs-api-tutorial/src/auth/auth.service.ts
@@ -1,14 +1,29 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2'
 
 @Injectable()
 export class AuthService {
 
 	constructor(private prisma: PrismaService) {}
 
-	signup() {
-		return { msg: 'I have signed up'};
+	async signup(dto: AuthDto) {
+		// generate password hash
+		const hash = await argon.hash(dto.password);
+		// save new user in db
+		const user = await this.prisma.user.create({
+			data: {
+				email: dto.email,
+				hash,
+			},
+		});
+
+		delete user.hash;
+		// return save user
+		return user;
 	}
+
 	signin() {
 		return { msg: 'I have signed in'};
 	}
--- ft_transcendance/nestjs-api-tutorial/src/main.ts
+++ ft_transcendance/nestjs-api-tutorial/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+//   Permet l'utilisation de Validation pipe (telecharger avec npm au préalable)
+  app.useGlobalPipes(new ValidationPipe({
+	whitelist: true, 
+	// whitelist permet de ne recevoir que les champs que nous avons definie (email, password)
+  }));
   await app.listen(3333);
 }
 bootstrap();
