f3bdd423461bc063b13d110f9cd68723dd20fe94
Gregoire GOBERT <ggobert@e1r1p9.clusters.42paris.fr>
2023-09-15 17:08:18 +0200

    nestjs tuto

---
+++ ft_transcendance/nestjs-api-tutorial/src/auth/auth.controler.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, ParseIntPipe, Post, Req } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { Request } from "express";
+import { AuthDto } from "./dto";
+
+@Controller('auth')
+export class AuthController{
+	constructor(private authService: AuthService) {}
+
+	@Post('signup')
+	signup(@Body() dto: AuthDto)
+	{
+		return this.authService.signup
+	}
+
+	@Post('signin')
+	signin()
+	{
+		return this.authService.signin
+	}
+}
