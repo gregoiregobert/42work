105db6019a17eb7e4446804320891993bacfe52d
Gregoire GOBERT <ggobert@e1r1p9.clusters.42paris.fr>
2024-03-21 12:11:37 +0100

    rendu cleaning

--- Piscine_Python_Data/Day_01/ex01/array2D.py
+++ Piscine_Python_Data/Day_01/ex01/array2D.py
@@ -3,20 +3,25 @@ import sys
 
 def slice_me(family: list, start: int, end: int) -> list:
 
+    try:
+        assert isinstance(family, list), \
+            "AssertionError: first argument must be a list"
+    except AssertionError as m:
+        print(m)
+        sys.exit(1)
+
     D1 = len(family)
     D2 = len(family[0])
 
     try:
+        assert isinstance(start, int), \
+            "AssertionError: second argument must be an int"
+        assert isinstance(end, int), \
+            "AssertionError: third argument must be an int"
         assert family[start:end], "AssertionError: list index out of range"
         for n in family:
             assert len(n) == D2, \
                 "AssertionError: lists are not the same size"
-        assert isinstance(family, list), \
-            "AssertionError: first argument must be a list"
-        assert isinstance(start, int), \
-            "AssertionError: second argument must be an int"
-        assert isinstance(end, int), \
-            "AssertionError: first argument must be an int"
     except AssertionError as m:
         print(m)
         sys.exit(1)
@@ -29,12 +34,14 @@ def slice_me(family: list, start: int, end: int) -> list:
     print(family[start:end])
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    list2 = [[1.5, 2],
-             [3, 4.88],
-             [5, 6],
-             [7.565, 8]]
+#     list_ = [[1.5, 2],
+#              [3, 4.88],
+#              [5, 6],
+#              [7.565, 8]]
+#     listError = 1
 
-    slice_me(list2, 2, 8)
-    # slice_me(list2, 0, -4)
+#     slice_me(list_, 2, 8)
+#     slice_me(list_, 0, -2)
+#     slice_me(list_, 0, list_)
