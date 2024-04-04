845310b061b9f3f34df8ce7a21b12bf3345e48fd
Gregoire GOBERT <ggobert@e1r1p9.clusters.42paris.fr>
2024-04-04 16:43:31 +0200

    don't remove the line with ports

--- Piscine_data_science/Day_00/ex02/table.py
+++ Piscine_data_science/Day_00/ex02/table.py
@@ -7,14 +7,15 @@ conn = psycopg2.connect(
     database="piscineds",
     user="ggobert",
     password="mysecretpassword",
-    host="0.0.0.0",
+    host=0
 )
 print("Connection success!")
 cur = conn.cursor()
 
 folder_path = '/mnt/nfs/homes/ggobert/Downloads/subject/customer'
 
-for file_name in os.listdir(folder_path)
-    if file_name == 
+for file_name in os.listdir(folder_path):
+    if file_name == 'data_2022_dec.csv':
+        print(os.path)
 
 conn.close()
