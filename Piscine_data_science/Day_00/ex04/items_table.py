421732878fe838f59ba8d8bdfc5230a4c8fef0ba
Gregoire GOBERT <ggobert@e1r1p9.clusters.42paris.fr>
2024-04-09 17:58:58 +0200

    day00 end

---
+++ Piscine_data_science/Day_00/ex04/items_table.py
@@ -0,0 +1,42 @@
+import psycopg2
+import os
+
+# Connecting to the Database
+print("Trying to connect to db")
+conn = psycopg2.connect(
+    database="piscineds",
+    user="ggobert",
+    password="mysecretpassword",
+    host=0
+)
+print("Connection success!")
+cur = conn.cursor()
+
+
+# Find the table's name
+folder_path = '/mnt/nfs/homes/ggobert/Downloads/subject/item'
+
+for file_name in os.listdir(folder_path):
+    if file_name.endswith('.csv'):
+        table_name = os.path.splitext(file_name)[0]
+
+        # Creating the table
+        cur.execute(f"""
+            CREATE TABLE IF NOT EXISTS {table_name} (
+                product_id INT,
+                category_id BIGINT,
+                caterogy_code TEXT,
+                brand TEXT
+            )
+        """)
+        print(f"Table {table_name} execute succefully")
+
+        # Open csv_files
+        with open(folder_path +  '/' + file_name, 'r') as csv_file:
+            next(csv_file)  # Skip header row
+            cur.copy_expert(f"COPY {table_name} FROM STDIN CSV HEADER", csv_file)
+        print(f"{file_name} copied succefully")
+
+conn.commit()
+cur.close()
+conn.close()
