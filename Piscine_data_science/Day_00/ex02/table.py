e45f2e2ec8e56a97a5d545aefdb04651a535a7ea
Gregoire GOBERT <ggobert@e1r1p9.clusters.42paris.fr>
2024-04-09 17:38:55 +0200

    day00 ex04

--- Piscine_data_science/Day_00/ex02/table.py
+++ Piscine_data_science/Day_00/ex02/table.py
@@ -1,7 +1,7 @@
 import psycopg2
 import os
 
-
+# Connecting to the Database
 print("Trying to connect to db")
 conn = psycopg2.connect(
     database="piscineds",
@@ -12,20 +12,33 @@ conn = psycopg2.connect(
 print("Connection success!")
 cur = conn.cursor()
 
+
+# Find the table's name
 folder_path = '/mnt/nfs/homes/ggobert/Downloads/subject/customer'
 
 for file_name in os.listdir(folder_path):
     if file_name == 'data_2022_dec.csv':
         table_name = os.path.splitext(file_name)[0]
 
-columns = [
-    "event_time TIMESTAMP",
-    "event_type TEXT",
-    "product_id INTEGER",
-    "price MONEY",
-    "user_id DECIMAL",
-    "user_session VARCHAR"
-]
+# Creating the table
+cur.execute(f"""
+    CREATE TABLE IF NOT EXISTS {table_name} (
+        event_time TIMESTAMP,
+        event_type TEXT,
+        product_id INTEGER,
+        price MONEY,
+        user_id DECIMAL,
+        user_session VARCHAR
+    )
+""")
+print("Table execute succefully")
 
+# Open CSV 
+with open(folder_path + '/data_2022_dec.csv', 'r') as csv_file:
+    next(csv_file)  # Skip header row
+    cur.copy_expert(f"COPY {table_name} FROM STDIN CSV HEADER", csv_file)
+print("Csv_file copied succefully")
 
+conn.commit()
+cur.close()
 conn.close()
